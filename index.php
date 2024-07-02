c9068de7001384b90c7f1c19aa63594babfb2457
Octavio <octaviorozas29@gmail.com>
2024-07-02 17:59:39 -0300

    trabajo php

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php 
+   session_start();
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Login</title>
+</head>
+<body>
+      <div class="container">
+        <div class="box form-box">
+            <?php 
+             
+              include("php/config.php");
+              if(isset($_POST['submit'])){
+                $email = mysqli_real_escape_string($con,$_POST['email']);
+                $password = mysqli_real_escape_string($con,$_POST['password']);
+
+                $result = mysqli_query($con,"SELECT * FROM users WHERE Email='$email' AND Password='$password' ") or die("Seleccionar error");
+                $row = mysqli_fetch_assoc($result);
+
+                if(is_array($row) && !empty($row)){
+                    $_SESSION['valid'] = $row['Email'];
+                    $_SESSION['username'] = $row['Username'];
+                    $_SESSION['age'] = $row['Age'];
+                    $_SESSION['id'] = $row['Id'];
+                }else{
+                    echo "<div class='message'>
+                      <p>Nombre de usuario o contraseña incorrectos</p>
+                       </div> <br>";
+                   echo "<a href='index.php'><button class='btn'>Volver</button>";
+         
+                }
+                if(isset($_SESSION['valid'])){
+                    header("Location: home.php");
+                }
+              }else{
+
+            
+            ?>
+            <header>Iniciar Sesión</header>
+            <form action="" method="post">
+                <div class="field input">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" autocomplete="off" required>
+                </div>
+
+                <div class="field input">
+                    <label for="password">Contraseña</label>
+                    <input type="password" name="password" id="password" autocomplete="off" required>
+                </div>
+
+                <div class="field">
+                    
+                    <input type="submit" class="btn" name="submit" value="Iniciar" required>
+                </div>
+                <div class="links">
+                    No tienes una cuenta? <a href="register.php">Regisrate ahora</a>
+                </div>
+            </form>
+        </div>
+        <?php } ?>
+      </div>
+</body>
+</html>
---
+++ php/config.php
@@ -0,0 +1,5 @@
+<?php 
+ 
+ $con = mysqli_connect("localhost","root","","proyecto") or die("Couldn't connect");
+
+?>
